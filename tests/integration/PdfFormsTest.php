04ca533d4557d648b0669269527aabd6dcfc161d
Jim Mullen <mullen2@iu.edu>
2024-06-28 11:27:48 -0400

    Fixed PDF test for changes in REDCap

--- tests/integration/PdfFormsTest.php
+++ tests/integration/PdfFormsTest.php
@@ -190,9 +190,16 @@ class PdfFormsTest extends TestCase
         # Check PDF generated for repeat instance 1
         #----------------------------------------------
         $text = preg_split("/\n/", $pdf->getText());
-        $this->assertEquals(9, count($text));
-        $this->assertEquals('Record ID 1001', $text[2]);
-        $this->assertEquals('Weight (lbs.) 173.4', preg_replace('/\s+/', ' ', $text[8]));
+
+        # Exact output appears to depend on REDCap version
+        if (count($text) === 11) {
+            $this->assertEquals('Record ID 1001', $text[3]);
+            $this->assertEquals('Weight (lbs.) 173.4', preg_replace('/\s+/', ' ', $text[10]));
+        } else {
+            $this->assertEquals(9, count($text));
+            $this->assertEquals('Record ID 1001', $text[2]);
+            $this->assertEquals('Weight (lbs.) 173.4', preg_replace('/\s+/', ' ', $text[8]));
+        }
 
         $repeatInstance = 2;
         $result = self::$repeatingFormsProject->exportPdfFileOfInstruments(
@@ -212,9 +219,16 @@ class PdfFormsTest extends TestCase
         # Check PDF generated for repeat instance 2
         #----------------------------------------------
         $text = preg_split("/\n/", $pdf->getText());
-        $this->assertEquals(9, count($text));
-        $this->assertEquals('Record ID 1001', $text[2]);
-        $this->assertEquals('Weight (lbs.) 172.4', preg_replace('/\s+/', ' ', $text[8]));
+
+        # Exact output appears to depend on REDCap version
+        if (count($text) === 11) {
+            $this->assertEquals('Record ID 1001', $text[3]);
+            $this->assertEquals('Weight (lbs.) 172.4', preg_replace('/\s+/', ' ', $text[10]));
+        } else {
+            $this->assertEquals(9, count($text));
+            $this->assertEquals('Record ID 1001', $text[2]);
+            $this->assertEquals('Weight (lbs.) 172.4', preg_replace('/\s+/', ' ', $text[8]));
+        }
 
         # delete imported test records
         $recordsDeleted = self::$repeatingFormsProject->deleteRecords([1001, 1002, 1003, 1004]);
